dae39f7ff4486598cddce48461f1e3e574aec3aa
rnod8 <411202097@qu.edu.sa>
2024-11-19 18:06:11 +0300

    make noteification page for provider and add order number

--- flutter_service4u_app/flutter_service4u_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_service4u_app/flutter_service4u_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\development\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\baria\home-maintenance\flutter_service4u_app\flutter_service4u_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\rnod8\homeMain\home-maintenance\flutter_service4u_app\flutter_service4u_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
